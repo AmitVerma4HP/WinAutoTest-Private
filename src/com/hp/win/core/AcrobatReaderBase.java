4244e16c45bd9c638c261e1dc510f361f5021b17
AmitVerma4HP <amit.verma5@hp.com>
2018-06-20 10:22:24 -0700

    PDF file open with Acrobat Reader
    PDF file open with Acrobat Reader

--- src/com/hp/win/core/AcrobatReaderBase.java
+++ src/com/hp/win/core/AcrobatReaderBase.java
@@ -10,8 +10,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
-import io.appium.java_client.windows.WindowsDriver;
-import io.appium.java_client.windows.WindowsElement;
+
 
 
 
--- src/com/hp/win/tests/PrintPdfFromAcrobat.java
+++ src/com/hp/win/tests/PrintPdfFromAcrobat.java
@@ -5,13 +5,11 @@ import java.io.IOException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.NoSuchElementException;
-import org.testng.Assert;
+
 import org.testng.annotations.*;
 
 import com.hp.win.core.AcrobatReaderBase;
-import com.hp.win.core.Base;
-import com.hp.win.core.MsWordAppBase;
+
 import com.hp.win.utility.PrintTraceCapture;
 import com.hp.win.utility.ScreenshotUtility;
 
@@ -22,24 +20,22 @@ public class PrintPdfFromAcrobat extends AcrobatReaderBase{
 	private static String currentClass;	
 
     @BeforeClass
-	@Parameters({"device_name", "ptr_name", "test_filename","word2016_exe_loc"})
-    public static void setup(String device_name, String ptr_name, @Optional("MicrosoftWord2016_Portrait_MultiPage_TestFile.docx")String test_filename,String word2016_exe_loc) 
+	@Parameters({"device_name", "ptr_name", "test_filename","acrobat_exe_loc"})
+    public static void setup(String device_name, String ptr_name, @Optional("File9.pdf")String test_filename,String acrobat_exe_loc) 
     		throws InterruptedException, IOException {
         	
     	currentClass = PrintPdfFromAcrobat.class.getSimpleName();
 		
 		//Start PrintTrace log capturing 
     	PrintTraceCapture.StartLogCollection(currentClass);	
-   		AcrobatSession = AcrobatReaderBase.OpenAcrobatReader(device_name, test_filename,word2016_exe_loc);    		
+   		AcrobatSession = AcrobatReaderBase.OpenAcrobatReader(device_name, test_filename,acrobat_exe_loc);    		
         Thread.sleep(1000); 
     }
 
 	
 	@Test
-	@Parameters({"device_name","ptr_name","paper_size","duplex_option","orientation","collation","copies","pages_to_print","page_count","margin","pages_per_sheet"})
-    public void PrintMsWordFile(String device_name,String ptr_name, @Optional("Letter")String paper_size,@Optional("Print One Sided")String duplex_option,
-    	@Optional("Portrait Orientation")String orientation,@Optional("Collated")String collation,@Optional("1")String copies,
-    	@Optional("Print All Pages")String pages_to_print, @Optional("NA")String page_count,@Optional("Normal")String margin,@Optional("1 Page Per Sheet")String pages_per_sheet) throws InterruptedException, IOException    {
+	@Parameters({"device_name","ptr_name"})
+    public void PrintPdfFile(String device_name,String ptr_name) throws InterruptedException, IOException    {
 		
 		AcrobatSession.getKeyboard().pressKey(Keys.CONTROL+"p");
 		log.info("Pressed CTRL+P to get to Print Option");
